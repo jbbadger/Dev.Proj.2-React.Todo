a416db6528e179fb13f1d7b131c30e27f563f805
jbmominee <jaymominee@gmail.com>
2018-09-16 15:45:59 -0500

    added input field with submit button to task list

--- rct-todo-app-1/src/App.js
+++ rct-todo-app-1/src/App.js
@@ -7,17 +7,30 @@ class App extends Component {
     super(props);
     this.state = {
       task: [{descriptor: "eat", isDone: true},
-             {descriptor: "sleep", isDone: false}]
+             {descriptor: "sleep", isDone: false}],
+            newTaskDescriptor: ''
     };
   }
 
   toggleComplete(index){
-    const newTasks = this.state.task.slice();
-    const nTask = newTasks[index];
-    nTask.isDone = nTask.isDone ? false : true;
-    this.setState({newTasks: newTasks});
+    const newTasks = this.state.task.slice(); // create new array of this.state
+    const nTask = newTasks[index]; //add index to new array as identifier
+    nTask.isDone = nTask.isDone ? false : true; //if array-index is checked, uncheck
+    this.setState({newTasks: newTasks}); //new state is sliced array
   }
-  
+
+  doSubmit(e) {
+    e.preventDefault();
+    if(!this.state.newTaskDescriptor){return};
+    const newSubmitTask = { descriptor: this.state.newTaskDescriptor, isDone: false};
+    this.setState({ task: [...this.state.task, newSubmitTask], newTaskDescriptor: '' });
+  }
+
+  makeChange(e) {
+    this.setState({ newTaskDescriptor: e.target.value })
+    console.log("change made");
+  }
+
   render() {
     return (
       <div className="App">
@@ -26,6 +39,10 @@ class App extends Component {
            <ToDo key={index} descriptor={item.descriptor} isDone={item.isDone} toggleComplete={() => this.toggleComplete(index)} />
          )}
         </ul>
+        <form onSubmit={ (e) => this.doSubmit(e) }>
+          <input type="text" value={ this.state.newTaskDescriptor } onChange={ (e) => this.makeChange(e) }/>
+          <input type="submit" />
+        </form>
       </div>
     );
   }
